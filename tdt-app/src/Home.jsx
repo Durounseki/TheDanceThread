ef8afb19c7a17235662a1e4229f01662e8f305fb
Durounseki <156300780+Durounseki@users.noreply.github.com>
2025-02-14 19:29:23 +0900

    feat: add home page

--- tdt-app/src/Home.jsx
+++ tdt-app/src/Home.jsx
@@ -1,5 +1,45 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import TDTLogo from "./TDTLogo.jsx";
+import Modal from "./Modal.jsx";
+import Login from "./Login.jsx";
+import useAuth from "./useAuth.jsx";
 const Home = () => {
-  return <h1>Home</h1>;
+  const { user } = useAuth();
+  const [showLogin, setShowLogin] = useState(false);
+  const navigate = useNavigate();
+  const handleNewEvent = (event) => {
+    if (user) {
+      navigate("/events/create");
+    } else {
+      event.preventDefault();
+      setShowLogin(true);
+    }
+  };
+  return (
+    <main className="home-page">
+      <h1>WELCOME TO</h1>
+      <TDTLogo />
+      <h2>Dance, Discover, Connect...</h2>
+      <div className="home-buttons">
+        <ul>
+          <li>
+            <Link to="/events">Start exploring</Link>
+          </li>
+          <li>
+            <a href="#" onClick={handleNewEvent}>
+              New event
+            </a>
+          </li>
+        </ul>
+      </div>
+      {showLogin && (
+        <Modal>
+          <Login closeLogin={setShowLogin} />
+        </Modal>
+      )}
+    </main>
+  );
 };
 
 export default Home;
--- tdt-app/src/Layout.jsx
+++ tdt-app/src/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Modal from "./Modal.jsx";
@@ -7,16 +7,18 @@ import Login from "./Login.jsx";
 
 const Layout = ({ loading }) => {
   const [showLogin, setShowLogin] = useState(false);
+  const location = useLocation();
+  const isHome = location.pathname === "/";
   return (
     <>
-      <Header showLogin={setShowLogin} />
-      {loading ? <p>Loading...</p> : <Outlet />}
+      {!isHome && <Header showLogin={setShowLogin} />}
+      {loading ? <p>Loading...</p> : <Outlet showLogin={setShowLogin} />}
       {showLogin && (
         <Modal>
           <Login closeLogin={setShowLogin} />
         </Modal>
       )}
-      <Footer showLogin={setShowLogin} />
+      {!isHome && <Footer showLogin={setShowLogin} />}
     </>
   );
 };
